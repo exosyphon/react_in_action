7f0dcf77ea36c2c45039b1b80e50a914f0f7a534
exosyphon <exosyphon@yahoo.com>
2016-12-18 14:53:16 -0700

    Explore lifecycle methods

---
+++ letters_social/components/app.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			posts: {
+				all: [],
+				filtered: [],
+			},
+			category: null,
+			filters: {
+				image: null,
+				link: null,
+				categories: [],
+			},
+			loaded: false,
+			showBanner: false,
+		};
+	}
+ render() { 
+ 	return (
+ 		<div className="app">
+ 		Letters Social — coming soon!
+ 		</div>
+ 		);
+ }
+}
